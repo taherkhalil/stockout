81715d458ca0781917ccf04bf9a1844b18630b4d
taherkhalil <taherkhalil52@gmail.com>
2018-01-01 17:16:29 +0300

    adding code to stockout

--- discard/discard/doctype/stock_out/stock_out.js
+++ discard/discard/doctype/stock_out/stock_out.js
@@ -6,3 +6,33 @@ frappe.ui.form.on('Stock OUT', {
 
 	}
 });
+
+frappe.ui.form.on	('items to discard',{
+	item: function(frm,cdt,cdn){
+		var d = locals[cdt][cdn];
+		frappe.call({
+		method: "discard.discard.doctype.discard_items.discard_items.get_valuation",
+		args: {
+			item_code: d.item
+		},
+		callback: function(r) {		
+			if(r.message) {
+				console.log(r.message);
+				d.valuation_rate =r.message;
+				cur_frm.refresh_field(d.valuation_rate);
+			} 
+		}
+	});
+	},
+
+	quantity:function(frm,cdt,cdn){
+		var d = locals[cdt][cdn];
+		quantity = d.quantity;
+		console.log(quantity);
+		amount = flt(quantity)*flt(d.valuation_rate);
+		console.log(amount);
+		d.amount = amount;
+		frm.refresh_field(d.amount);
+	}
+
+});
--- discard/discard/doctype/stock_out/stock_out.py
+++ discard/discard/doctype/stock_out/stock_out.py
@@ -7,4 +7,18 @@ import frappe
 from frappe.model.document import Document
 
 class StockOUT(Document):
-	pass
+	def on_submit(self):
+		frappe.errprint("in on on_submit")
+		# it = self.get('items')
+		# frappe.errprint(it)
+		se = frappe.new_doc("Stock Entry")
+		se.purpose = "Material Receipt" 
+		for items in self.get('items'):
+			row = se.append("items",{})
+			row.t_warehouse =items.warehouse
+			row.item_code = items .item
+			row.qty= -items.quantity
+			expense_account =self.expense_account
+		se.insert(ignore_permissions=True)
+		se.submit()
+	
